2ed38c3a1a6be31c08c85cd4596645aedb7acae6
POP CODE <lolo4ka69@list.ru>
2023-01-09 15:39:18 +0300

    IMT +

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-       BmiService service = new BmiService();
-       int BMI = service.calculate(1.75,80);
-       System.out.println(BMI);
+        BmiService service = new BmiService();
+        int BMI = service.calculate(1.75, 80);
+        System.out.println(BMI);
     }
 }
